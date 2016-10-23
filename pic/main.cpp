7e7cae72605236170dccf4909d6bc08883a343c5
Alexander Gaivanuk <alexgaiv0748@gmail.com>
2016-10-23 21:06:52 +0400

    add relative errors

--- pic/main.cpp
+++ pic/main.cpp
@@ -14,17 +14,16 @@ ostream &operator<<(ostream &os, const Vector3d &v)
 	return os;
 }
 
-void Test1()
+void Test1(int steps)
 {
 	Particle p;
 	p.mass = electronMass;
 	p.charge = electronCharge;
 
-	const int N = 100;
 	const double mc = p.mass * c;
 	const double E0 = 43;
 
-	cout << "\n\nTest 1 (N = " << N << ")\n\n";
+	cout << "\n\nTest 1 (N = " << steps << ")\n\n";
 
 	for (int i = 0; i < 3; i++) // for each axis
 	{
@@ -36,33 +35,35 @@ void Test1()
 			double Ek = E0 * k;
 			E[i] = Ek;
 
-			double dt = mc / (p.charge * Ek * N);
+			double dt = mc / (p.charge * Ek * steps);
 
-			Particle p1 = ParticleMover(E, B).MoveParticle(p, dt, N);
+			Particle p1 = ParticleMover(E, B).MoveParticle(p, dt, steps);
 
 			Vector3d r_theor;
 			r_theor[i] = mc * c / (p.charge * Ek) * (sqrt(2.0) - 1.0);
 			Vector3d p_theor;
 			p_theor[i] = mc;
 
-			double r_error = (p1.coords - r_theor).Length();
-			double p_error = (p1.momentum - p_theor).Length();
+			double r_error_abs = (p1.coords - r_theor).Length();
+			double p_error_abs = (p1.momentum - p_theor).Length();
+			double r_error_rel = r_error_abs / r_theor.Length();
+			double p_error_rel = p_error_abs / p_theor.Length();
 
 			cout << "E = " << E << endl;
 			cout << "r = " << r_theor << ", p = " << p_theor << endl;
 			cout << "r = " << p1.coords << ", p = " << p1.momentum << endl;
-			cout << "r_error = " << r_error << ", p_error = " << p_error << "\n\n";
+			cout << "r_error_abs = " << r_error_abs << ", p_error_abs = " << p_error_abs << endl;
+			cout << "r_error_rel = " << r_error_rel << ", p_error_rel = " << p_error_rel << "\n\n";
 		}
 	}
 }
 
-void Test2()
+void Test2(int steps)
 {
 	Particle p;
 	p.mass = electronMass;
 	p.charge = electronCharge;
 
-	const int N = 100;
 	const double mc = p.mass * c;
 	const double p0 = 5;
 	const double B0 = 57;
@@ -71,27 +72,30 @@ void Test2()
 	Vector3d B(0, 0, B0);
 	p.momentum = Vector3d(p0, 0, 0);
 
-	double dt = M_PI * mc / (abs(p.charge) * B0 * N) * sqrt(1 + pow(p0 / mc, 2));
+	double dt = M_PI * mc / (abs(p.charge) * B0 * steps) * sqrt(1 + pow(p0 / mc, 2));
 
-	Particle p1 = ParticleMover(E, B).MoveParticle(p, dt, N);
+	Particle p1 = ParticleMover(E, B).MoveParticle(p, dt, steps);
 
 	Vector3d r_theor(0, -2 * p0*c / (p.charge * B0), 0);
 	Vector3d p_theor(-p0, 0, 0);
 
-	double r_error = (p1.coords - r_theor).Length();
-	double p_error = (p1.momentum - p_theor).Length();
+	double r_error_abs = (p1.coords - r_theor).Length();
+	double p_error_abs = (p1.momentum - p_theor).Length();
+	double r_error_rel = r_error_abs / r_theor.Length();
+	double p_error_rel = p_error_abs / p_theor.Length();
 
-	cout << "\n\nTest 2 (N = " << N << ")\n\n";
+	cout << "\n\nTest 2 (N = " << steps << ")\n\n";
 	cout << "B = " << B << endl;
 	cout << "r = " << r_theor << ", p = " << p_theor << endl;
 	cout << "r = " << p1.coords << ", p = " << p1.momentum << endl;
-	cout << "r_error = " << r_error << ", p_error = " << p_error << "\n\n";
+	cout << "r_error_abs = " << r_error_abs << ", p_error_abs = " << p_error_abs << endl;
+	cout << "r_error_rel = " << r_error_rel << ", p_error_rel = " << p_error_rel << "\n\n";
 }
 
 int main()
 {
-	Test1();
-	Test2();
+	Test1(100);
+	Test2(100);
 	getchar();
 	return 0;
 }
